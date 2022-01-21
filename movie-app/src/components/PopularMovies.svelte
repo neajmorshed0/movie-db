0f27eec1dbd7323ba65e99e77cfd9df9cbc64856
neajmorshed0 <neajmorshedimon@gmail.com>
2022-01-22 02:04:04 +0600

    initial commit

---
+++ movie-app/src/components/PopularMovies.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let popular;
+  import MovieCard from "./MovieCard.svelte";
+</script>
+
+<h3>Popular Movies</h3>
+
+<div class="popular-movies">
+  {#each popular as movie}
+    <MovieCard {movie} />
+  {/each}
+</div>
---
+++ movie-app/src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=0c130c34bc7de50f48c687c8e7e4523e`
+    );
+    const data = await res.json();
+    console.log(data);
+    if (res.ok) {
+      return {
+        props: { popular: data.results },
+      };
+    }
+  }
+</script>
+
+<script>
+  import PopularMovies from "../components/PopularMovies.svelte";
+  export let popular;
+</script>
+
+<section>
+  <PopularMovies {popular} />
+</section>
